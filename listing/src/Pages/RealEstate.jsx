c2f12b585fc360bae80b247aa85aa6a953b0e66b
Giltr749 <97340782+Giltr749@users.noreply.github.com>
2022-05-24 13:02:54 +0300

    Added price ascending/descending

--- listing/src/Pages/RealEstate.jsx
+++ listing/src/Pages/RealEstate.jsx
@@ -1,21 +1,42 @@
 import React, { useEffect } from 'react';
 import MainCard from '../Components/MainCard';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import SearchInput from '../Components/SearchInput';
+import ListingContext from '../contexts/ListingsContext';
 
 function RealEstate(props) {
+
+    const [propertiesData, setPropertiesData] = useContext(ListingContext);
+    const [rawData, setRawData] = useContext(ListingContext);
 
     const [addressInput, setAddressInput] = useState('');
     const [roomsInput, setRoomsInput] = useState(1);
     const [priceAscend, setPriceAscend] = useState(true);
+    
+    //Sorts according to price ascending/descending
+    useEffect(() => {
+        let tempArr = propertiesData;
+        priceAscend
+            ? tempArr.sort((a, b) => {
+                if (a.price < b.price)
+                    return 1;
+                else if (a.price > b.price)
+                    return -1;
+            })
+            : tempArr.sort((a, b) => {
+                if (a.price > b.price)
+                    return 1;
+                else if (a.price < b.price)
+                    return -1;
+            })
 
-    useEffect(()=>{
-        
-    },[priceAscend])
+            setPropertiesData(tempArr);
+            // console.log(tempArr);
+    }, [priceAscend])
 
     return (
         <div>
-            <SearchInput address={{addressInput, setAddressInput}} rooms={{roomsInput, setRoomsInput}} price={{priceAscend, setPriceAscend}}/>
+            <SearchInput address={{ addressInput, setAddressInput }} rooms={{ roomsInput, setRoomsInput }} price={{ priceAscend, setPriceAscend }} />
             <MainCard />
         </div>
     );
